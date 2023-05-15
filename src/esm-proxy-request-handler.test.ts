522f3011ee2189e19759410ed923f618d09e597b
Carles Escrig Royo <esroyo@gmail.com>
2023-05-15 13:50:16 +0200

    fix: will forward the request to esm.sh keeping parameters

--- src/esm-proxy-request-handler.test.ts
+++ src/esm-proxy-request-handler.test.ts
@@ -1,14 +1,25 @@
 import { assertEquals } from 'testing/asserts';
-import { returnsNext, stub } from 'testing/mock';
+import { assertSpyCallArg, returnsNext, stub } from 'testing/mock';
 import { esmProxyRequestHandler } from './esm-proxy-request-handler.ts';
 
 Deno.test('should return an string of code in systemjs format', async () => {
   const fetchStub = stub(globalThis, 'fetch', returnsNext([
       Promise.resolve({ text: () => Promise.resolve('export * from "https://esm.sh/stable/vue@3.3.2/es2022/vue.mjs";') } as unknown as Response),
   ]));
-  const req = new Request('https://esm.sh/vue');
+  const req = new Request('https://systemjs.sh/vue');
   const res = await esmProxyRequestHandler(req);
   const systemjsCode = await res.text();
   assertEquals(systemjsCode.startsWith('System.register('), true);
+  fetchStub.restore();
+});
+
+Deno.test('should forward the request to host "esm.sh" keeping the parameters', async () => {
+  const fetchStub = stub(globalThis, 'fetch', returnsNext([
+      Promise.resolve({ text: () => Promise.resolve('export default "foo"') } as unknown as Response),
+  ]));
+  const req = new Request('https://systemjs.sh/foo?bundle');
+  const res = await esmProxyRequestHandler(req);
+  const systemjsCode = await res.text();
+  assertSpyCallArg(fetchStub, 0, 0, 'https://esm.sh/foo?bundle');
   fetchStub.restore();
 });
--- src/esm-proxy-request-handler.ts
+++ src/esm-proxy-request-handler.ts
@@ -1,15 +1,19 @@
 import { toSystemjs } from './to-systemjs.ts';
 
-export async function esmProxyRequestHandler(req: Request) {
+export async function esmProxyRequestHandler(req: Request): Promise<Response | never> {
   let modifiedUrl: URL;
   let esmCode: string;
   try {
-    modifiedUrl = new URL(req.url, 'https://esm.sh');
+    modifiedUrl = new URL(req.url);
+    modifiedUrl.hostname = 'esm.sh';
     esmCode = await fetch(modifiedUrl.toString(), { headers: req.headers }).then((res) => res.text());
     const systemjsCode = await toSystemjs(esmCode);
     return new Response(systemjsCode);
   } catch (error) {
+    // @ts-ignore
     console.log(modifiedUrl?.toString());
+    // @ts-ignore
     console.log(esmCode);
+    throw error;
   }
 }
