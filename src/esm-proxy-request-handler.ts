7e3820caf70f1c278e47b0c5f7b17670275203d2
Carles Escrig Royo <esroyo@gmail.com>
2023-05-15 13:31:22 +0200

    fix: add error log

--- src/esm-proxy-request-handler.ts
+++ src/esm-proxy-request-handler.ts
@@ -1,8 +1,15 @@
 import { toSystemjs } from './to-systemjs.ts';
 
 export async function esmProxyRequestHandler(req: Request) {
-  const modifiedUrl = new URL(req.url, 'https://esm.sh');
-  const esmCode = await fetch(modifiedUrl.toString(), { headers: req.headers }).then((res) => res.text());
-  const systemjsCode = await toSystemjs(esmCode);
-  return new Response(systemjsCode);
+  let modifiedUrl: URL;
+  let esmCode: string;
+  try {
+    modifiedUrl = new URL(req.url, 'https://esm.sh');
+    esmCode = await fetch(modifiedUrl.toString(), { headers: req.headers }).then((res) => res.text());
+    const systemjsCode = await toSystemjs(esmCode);
+    return new Response(systemjsCode);
+  } catch (error) {
+    console.log(modifiedUrl?.toString());
+    console.log(esmCode);
+  }
 }
