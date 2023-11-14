7e3efd92c0fc277876cb1ef7a2c4619d1de0ec62
Carles Escrig Royo <esroyo@gmail.com>
2023-11-14 19:58:15 +0100

    chore: update deps to use npm specifiers

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 import 'https://deno.land/std/dotenv/mod.ts';
-export { default as rollupPluginVirtual } from 'https://cdn.skypack.dev/@rollup/plugin-virtual';
-export type { InputOptions, ModuleFormat, OutputOptions } from 'https://esm.sh/rollup';
-export { rollup } from 'https://esm.sh/rollup';
+export { default as rollupPluginVirtual } from 'npm:@rollup/plugin-virtual@3.0.2';
+export type { InputOptions, ModuleFormat, OutputOptions } from 'npm:rollup@3.29.4';
+export { rollup } from 'npm:rollup@3.29.4';
 export { load as dotenvLoad } from 'https://deno.land/std/dotenv/mod.ts';
-export { default as httpz } from 'https://cdn.skypack.dev/http-z@6.1.2';
+export { default as httpz } from 'npm:http-z@6.1.2';
 export { serve } from 'https://deno.land/std/http/mod.ts';
 //export { default as request } from 'https://esm.sh/request@2.88.2';
