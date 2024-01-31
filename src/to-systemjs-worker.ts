e8be08b8f1fff4308b6c244c57afa533770f59bc
Carles Escrig Royo <esroyo@gmail.com>
2024-01-31 12:46:26 +0100

    fix: avoid Worker recursion

--- src/to-systemjs-worker.ts
+++ src/to-systemjs-worker.ts
@@ -1,8 +1,9 @@
-import { toSystemjs } from './to-systemjs.ts';
+import { toSystemjsMain } from './to-systemjs.ts';
 
 // @ts-ignore
 self.addEventListener('message', async (event: MessageEvent<{ args: Parameters<typeof toSystemjs> }>) => {
-    const transpiledCode = await toSystemjs(...event.data.args);
+    // @ts-ignore
+    const transpiledCode = await toSystemjsMain(...event.data.args);
     // @ts-ignore
     self.postMessage({ code: transpiledCode });
     // @ts-ignore
