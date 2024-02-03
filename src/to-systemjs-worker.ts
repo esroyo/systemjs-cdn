a6775742799b2439edd7a0807580ee1767b930e3
Carles Escrig Royo <esroyo@gmail.com>
2024-02-03 10:29:33 +0100

    chore: fix formatting

--- src/to-systemjs-worker.ts
+++ src/to-systemjs-worker.ts
@@ -3,7 +3,9 @@ import { toSystemjsMain } from './to-systemjs.ts';
 // @ts-ignore
 self.addEventListener(
     'message',
-    async (event: MessageEvent<{ args: Parameters<typeof toSystemjsMain> }>) => {
+    async (
+        event: MessageEvent<{ args: Parameters<typeof toSystemjsMain> }>,
+    ) => {
         // @ts-ignore
         const transpiledCode = await toSystemjsMain(...event.data.args);
         // @ts-ignore
